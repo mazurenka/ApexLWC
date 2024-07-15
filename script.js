e62fa535a4a8c1c867f141330a7e22dc4f72c09a
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-15 10:33:40 +0100

    10

---
+++ script.js
@@ -0,0 +1,31 @@
+// var
+
+var cource = "zero to hero"
+console.log(cource)
+var cource = "zero to hero in LWC"
+console.log(cource)
+
+function abc() {
+    var anotherCource = "hero in LWC"
+    console.log(anotherCource)
+}
+
+abc()
+
+//console.log(anotherCource) //error
+
+if (2 === 2) {
+    var x = 10
+}
+console.log(x) //x = 10
+
+
+
+
+
+
+
+
+
+
+
---
+++ script1.js
@@ -0,0 +1,62 @@
+var x = 10.66
+console.log(typeof x) //number
+
+var y = 34523451345n
+console.log(typeof y) //bigint
+
+var userName = "Andre"
+console.log(typeof userName) //string
+
+var isMonday = true
+console.log(typeof isMonday) //boolean
+
+var q 
+console.log(typeof q) //undefined
+
+var obj = {}
+console.log(typeof obj) //object
+
+var arr = []
+console.log(typeof arr) //object
+
+var sym = Symbol("id")
+console.log(typeof sym) //symbol
+
+var x1 = 10
+x1 = null
+console.log(typeof x1) //object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
