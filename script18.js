3ee48521f26d4bbbe575cc7090434f7242eb6337
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-16 18:09:36 +0100

    18

--- script18.js
+++ script18.js
@@ -1,64 +1,27 @@
-let arr = [2, 3, 5, 7, 9, 10]
 
-// map()
-let newArray = arr.map(function(currentItem, index, array){
-    return currentItem*2
+function checkIsSuccess(data) {
+    return new Promise(function (resolve, reject) {
+        if (data === "success") {
+            return resolve("successfully executed")
+        } else {
+            return reject("unsuccessfully executed")
+        }
+    })
+}
+
+checkIsSuccess('success').then(function (result) {
+   console.log(result) 
+}).catch(function (error) {
+    console.error(error)
 })
-console.log(arr)
-console.log(newArray)
 
-// filter()
-let filteredArr = arr.filter(function (currentItem, index, array) {
-    return currentItem < 5
+console.log(checkIsSuccess('success'))
+
+fetch('https://api.github.com/users/mazurenka').then(function(result) {
+    return result.json()
+}).then(function (response) {
+    console.log(response)
 })
-console.log(arr)
-console.log(filteredArr)
-
-// every()
-let age = [32, 33, 18, 40]
-let isAllAdult = age.every(function (currentItem) {
-    return currentItem <= 18
-})
-console.log(isAllAdult)
-
-
-//some
-let ageList = [32, 33, 18, 40]
-let isAllAdultList = ageList.some(function (currentItem) {
-    return currentItem < 32
-})
-console.log(isAllAdultList)
-
-
-//sort
-let names = ["John", "Andre", "Harry"]
-console.log(names.sort())
-
-//sorting of numbers
-let points = [10, 15, 3, 99, 27]
-let sortedPoints = points.sort(function (a, b) {
-    return a-b
-})
-console.log(sortedPoints)
-
-
-//reduce
-let num = [2, 8, 10]
-let sum = num.reduce(function (total, currentItem) {
-    return total + currentItem
-})
-console.log(sum)
-
-
-//forEach()
-num.forEach(function (currentItem) {
-    console.log(currentItem+1)
-})
-console.log(num)
-
-
-
-
 
 
 
