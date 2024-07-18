dc2b760806f681798f5122f1d0a60266119b8d01
Andrei Mazurenka <mazurenka.pl@gmail.com>
2024-07-18 12:25:48 +0100

    21

--- script21.js
+++ script21.js
@@ -1,14 +1,26 @@
 
 
-function functionOnclick() {
-    console.log("Click Event")
+
+
+
+
+document.addEventListener("hello", function(data) {
+    console.log("hello has called and send", data.detail)
+})
+
+function callCustomMethod() {
+    let event = new CustomEvent("hello", {
+        detail:{name:"andre"}
+    })
+    document.dispatchEvent(event)
 }
 
+// function functionOnclick() {
+//     console.log("Click Event")
+// }
 
 /*let btn = document.querySelector("button")
 btn.addEventListener("click", onClickFoo)*/
-
-
 
 
 
